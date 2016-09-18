ee9795abf41223e4adcbee2b1507f19cf3dc0a66
Belal Khan <Belal@MACs-MacBook-Air.local>
2016-09-18 20:58:42 +0530

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/navigationdrawerexample/Menu1.java
@@ -0,0 +1,32 @@
+package net.simplifiedcoding.navigationdrawerexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Belal on 18/09/16.
+ */
+
+
+public class Menu1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.fragment_menu_1, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Menu 1");
+    }
+}
